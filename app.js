bccd79be697ea4f4177ea3118716dfb602dd6caa
Yanira-AB <arenazas2222@gmail.com>
2019-11-22 13:31:16 -0500

    extrayendo info de data para mostrar en el modal

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
+import potter from './data/potter.js';
+
+const arrayMap = (data) => {
+    let id = 0;
+    const arrayCopy = data.map((element)=>({
+        id: id += 1,
+        nombre: element.name,
+    }));
+    return arrayCopy;
+};
+
 const close = document.getElementById('close');
+const info = document.getElementById('contenido');
 const modal = document.getElementById('modal');
-const flex = document.getElementById('flex');
 const btnModal = document.getElementById('btnModal');
+const btnCards = document.getElementById('btnCards');
+const cardsSection = document.getElementById('cards');
+
 const closeModal = () => {
     modal.style.display = 'none';
 };
-const showModal = () => {
+const showModal = (contenido) => {
+    info.innerHTML= '';
+    console.log(arrayMap(potter));
+    info.innerHTML= contenido;
     modal.style.display = 'flex';
+};
+const closeGrey = () => {
+    console.log(event.target);
+
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
+
+const createCards = (data) => {
+    cardsSection.innerHTML='';
+    data.forEach(element => {
+    const div = document.createElement('div');
+    div.setAttribute('id',`${element.id}`);
+    div.innerHTML = 
+    `<ul>
+    <li class="pointer">${element.nombre}</li>
+    </ul>`;
+    cardsSection.appendChild(div);
+    document.getElementById(element.id).addEventListener('click', () => { showModal(element.nombre)});
+    });
 };
 
 close.addEventListener('click', closeModal);
-btnModal.addEventListener('click', showModal);
+btnModal.addEventListener('click',  () => { showModal('Holi :)')});
+window.addEventListener('click', closeGrey);
+btnCards.addEventListener('click', () => { createCards(arrayMap(potter))});
